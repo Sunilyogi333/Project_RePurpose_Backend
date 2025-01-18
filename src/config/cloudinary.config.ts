396ba08f2a3f3818f0c04affa0ffa11cc24634b7
Sunilyogi333 <sunilnath0109@gmail.com>
2025-01-18 15:33:09 +0545

    Update Cloudinary configuration to use environment variables directly

--- src/config/cloudinary.config.ts
+++ src/config/cloudinary.config.ts
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME || 'dwaf3uzd5',
-  api_key: process.env.CLOUD_API_KEY || '121557529677688',
-  api_secret: process.env.CLOUD_API_SECRET || 'O8IhTeXjx8e8EKOGqXKHiLsE79w',
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
 // console.log(cloudinary);
